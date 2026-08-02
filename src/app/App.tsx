7d8eaa95da43d8b97985fc4100e0e46b80631b1c
frank <farruxvloging@gmail.com>
2026-08-03 06:37:31 +0900

    feat: publish authentication modal companent

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,16 +8,24 @@ import HomeNavbar from "./components/headers/HomeNavbar";
 import OtherNavbar from "./components/headers/OtherNavbar";
 import Footer from "./components/footer";
 import HelpPage from "./screens/helpPage";
-import "../css/app.css";
-import "../css/navbar.css";
-import "../css/footer.css";
 import { json } from "stream/consumers";
 import { LocalAtmRounded } from "@mui/icons-material";
 import useBasket from "./hooks/useBasket";
+import AuthenticationModal from "./components/auth";
+import "../css/app.css";
+import "../css/navbar.css";
+import "../css/footer.css";
 
 function App() {
   const location = useLocation();
   const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = useBasket();
+  const [signupOpen, setSignupOpen] = useState<boolean>(false);
+  const [loginOpen, setLoginOpen] = useState<boolean>(false);
+
+  /** HANDLERS */
+
+  const handleSignupClose = () => setSignupOpen(false);
+  const handleLoginClose = () => setLoginOpen(false);
 
   return (
     <>
@@ -56,6 +64,13 @@ function App() {
         </Route>
       </Switch>
       <Footer />
+
+      <AuthenticationModal
+        signupOpen={signupOpen}
+        loginOpen={loginOpen}
+        handleLoginClose={handleLoginClose}
+        handleSignupClose={handleLoginClose}
+      />
     </>
   );
 }
