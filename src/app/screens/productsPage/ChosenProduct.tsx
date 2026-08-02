f66fed5b4f52b7cdf8cdd8b34f68acb0726d1b08
frank <farruxvloging@gmail.com>
2026-08-02 21:18:01 +0900

    feat: develop logic of adding products into basket

--- src/app/screens/productsPage/ChosenProduct.tsx
+++ src/app/screens/productsPage/ChosenProduct.tsx
@@ -22,6 +22,7 @@ import { Member } from "../../../lib/types/member";
 import { serverApi } from "../../../lib/config";
 import ProductService from "../../services/ProductService";
 import MemberService from "../../services/MemberService";
+import { CartItem } from "../../../lib/types/search";
 
 /** REDUX SLICE & SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
@@ -40,8 +41,11 @@ const restaurantRetriever = createSelector(
     restaurant,
   }),
 );
-
-export default function ChosenProduct() {
+interface ChosenProductProps {
+  onAdd: (item: CartItem) => void;
+}
+export default function ChosenProduct(props: ChosenProductProps) {
+  const { onAdd } = props;
   const { productId } = useParams<{ productId: string }>();
   const { setRestaurant, setChosenProduct } = actionDispatch(useDispatch());
   const { chosenProduct } = useSelector(chosenProductRetriever);
