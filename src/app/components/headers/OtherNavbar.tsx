069894ead89d580c6e17027de1b98c3527b55ad7
frank <farruxvloging@gmail.com>
2026-08-03 19:11:15 +0900

    feat: create useGlobals integrated with Context & develop logout process

--- src/app/components/headers/OtherNavbar.tsx
+++ src/app/components/headers/OtherNavbar.tsx
@@ -1,7 +1,9 @@
-import { Box, Button, Container, Stack } from "@mui/material";
+import { Box, Button, Container, ListItemIcon, Menu, MenuItem, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import Basket from "./Basket";
 import { CartItem } from "../../../lib/types/search";
+import { useGlobals } from "../../hooks/useGlobals";
+import { Logout } from "@mui/icons-material";
 
 interface OtherNavbarProps {
   cartItems: CartItem[];
@@ -11,7 +13,12 @@ interface OtherNavbarProps {
   onDeleteAll: () => void;
   setSignupOpen: (isOpen: boolean) => void;
   setLoginOpen: (isOpen: boolean) => void;
+   handleLogoutClick: (e: React.MouseEvent<HTMLElement>) => void;
+  anchorEl: HTMLElement | null;
+  handleCLoseLogout: () => void;
+  handleLogoutRequest: () => void;
 }
+
 
 export default function OtherNavbar(props: OtherNavbarProps) {
   const {
@@ -22,8 +29,12 @@ export default function OtherNavbar(props: OtherNavbarProps) {
     onDeleteAll,
     setSignupOpen,
     setLoginOpen,
+    handleLogoutClick,
+    anchorEl,
+    handleCLoseLogout,
+    handleLogoutRequest,
   } = props;
-  const authMember = null;
+  const { authMember } = useGlobals();
 
   return (
     <div className="other-navbar">
@@ -82,11 +93,57 @@ export default function OtherNavbar(props: OtherNavbarProps) {
               </Box>
             ) : (
               <img
-                className="user-avatar"
-                src={"/icons/default-user.svg"}
+                 className="user-avatar"
+                src={
+                  authMember?.memberImage
+                    ? `${authMember?.memberImage}`
+                    : "/icons/default-user.svg"
+                }
                 aria-haspopup={"true"}
+                onClick={handleLogoutClick}
               />
             )}
+            <Menu
+              id="account-menu"
+              open={Boolean(anchorEl)}
+              onClose={handleCLoseLogout}
+              onClick={handleCLoseLogout}
+              PaperProps={{
+                elevation: 0,
+                sx: {
+                  overflow: "visible",
+                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                  mt: 1.5,
+                  "& .MuiAvatar-root": {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
+                  },
+                  "&:before": {
+                    content: '""',
+                    display: "block",
+                    position: "absolute",
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: "background.paper",
+                    transform: "translateY(-50%) rotate(45deg)",
+                    zIndex: 0,
+                  },
+                },
+              }}
+              transformOrigin={{ horizontal: "right", vertical: "top" }}
+              anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+            >
+              <MenuItem onClick={handleLogoutRequest}>
+                <ListItemIcon>
+                  <Logout fontSize="small" style={{ color: "blue" }} />
+                </ListItemIcon>
+                Logout
+              </MenuItem>
+            </Menu>
           </Stack>
         </Stack>
       </Container>
